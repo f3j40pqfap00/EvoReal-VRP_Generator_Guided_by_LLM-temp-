08078b02b1abbc1258ef1cf8926174f4c583251f
Henry <smyjz6_zjh@163.com>
2025-08-05 19:01:57 +0800

    Update utils.py

--- POMO-EvoReal/CVRP/POMO_CVRP_p1/POMO/utils.py
+++ POMO-EvoReal/CVRP/POMO_CVRP_p1/POMO/utils.py
@@ -146,7 +146,7 @@ class TimeEstimator:
             count, total, elapsed_time_str, remain_time_str))
 
 
-
+# Functions below are added  by author
 def extract_file_number(folder_name):
     "Custom sort function: Extracts the first numeric value in the name and sorts by numeric value"
     numbers = re.findall(r'\d+', folder_name)  # Extract all numbers
